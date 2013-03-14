01e8802b784fc8b92edd1ee5b6d6426711bfd7e8
TJ Holowaychuk <tj@vision-media.ca>
2013-03-14 21:16:52 +0000

    add --direct support

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var client = new Cloudup({
 exports.collection = function(options){
   var col = client.collection(options);
   var Reporter = require('./lib/' + options.reporter);
-  var reporter = new Reporter(col, { remote: remote });
+  var reporter = new Reporter(col, { remote: remote, direct: options.direct });
   return col;
 };
 
