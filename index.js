611e1132cda79774dd44f8435601598a63833ea7
TJ Holowaychuk <tj@vision-media.ca>
2013-03-14 20:50:35 +0000

    add console reporter

--- index.js
+++ index.js
@@ -5,9 +5,7 @@
  */
 
 var Cloudup = require('cloudup-client')
-  , sprintf = require('printf')
-  , path = require('path')
-  , basename = path.basename;
+  , console = require('./lib/console');
 
 /**
  * Cloudup remote.
@@ -26,12 +24,6 @@ var client = new Cloudup({
 });
 
 /**
- * Item progress row index.
- */
-
-var y = 0;
-
-/**
  * Upload collection.
  *
  * @param {Object} options
@@ -41,119 +33,8 @@ var y = 0;
 
 exports.collection = function(options){
   var col = client.collection(options);
-
-  // track progress
-  progress(col);
-
-  // setup
-  console.log();
-  clear();
-  hideCursor();
-
-  // saved
-  col.on('save', function(){
-    move(0, 2 + y);
-    log('collection', remote + '/' + col.uid);
-  });
-
-  // done
-  col.on('end', function(){
-    move(0, 3 + y);
-    console.log();
-  });
-
+  console(col);
   return col;
 };
 
-/**
- * Track progress.
- */
-
-function progress(col) {
-  col.on('item', function(item){
-    var cy = y++;
-
-    function ctx() {
-      return truncate(item._file
-        ? basename(item._file)
-        : item._url);
-    }
-
-    process.nextTick(function(){
-      move(0, 2 + cy);
-      log(ctx(), '0%');
-    });
-
-    item.on('progress', function(e){
-      var n = e.percent | 0;
-      move(0, 2 + cy);
-      log(ctx(), n + '%');
-    });
-
-    item.on('end', function(){
-      move(0, 2 + cy);
-      log(ctx(), remote + '/' + col.uid + '/' + item.uid);
-    });
-  });
-}
-
-// restore
-
-process.on('exit', showCursor);
-process.on('SIGINT', showCursor);
-process.on('SIGINT', clear);
-process.on('SIGINT', process.exit.bind(null, 1));
-
-/**
- * Clear the term.
- */
-
-function clear() {
-  process.stdout.write('\033[0m\033[1J');
-  process.stdout.write('\033[J');
-}
-
-/**
- * Hide the cursor.
- */
-
-function hideCursor(){
-  process.stdout.write('\033[?25l');
-}
-
-/**
- * Show the cursor.
- */
-
-function showCursor(){
-  process.stdout.write('\033[?25h');
-}
-
-/**
- * Move to (x, y).
- */
-
-function move(x, y) {
-  x = Math.round(x);
-  y = Math.round(y);
-  process.stdout.write('\033[' + y + ';' + x + 'H');
-}
-
-/**
- * Truncate `str`.
- */
-
-function truncate(str) {
-  var w = 20;
-  if (str.length < w) return str;
-  return str.slice(0, w) + '…';
-}
-
-/**
- * Log `key` / `str`.
- */
-
-function log(key, str) {
-  process.stdout.write(sprintf('\033[36m%25s\033[0m : \033[90m%s\033[m\n', key, str));
-}
 
