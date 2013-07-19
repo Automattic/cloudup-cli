0000836dd551b4c8e0c59eaf46802d42cace0db1
TJ Holowaychuk <tj@vision-media.ca>
2013-07-19 18:23:12 +0000

    refactor with .client()

--- index.js
+++ index.js
@@ -5,24 +5,34 @@
  */
 
 var Cloudup = require('cloudup-client');
+var pkg = require('./package');
+
+/**
+ * Create a client with `opts`.
+ *
+ * @param {Object} opts
+ * @return {Cloudup}
+ * @api private
+ */
+
+exports.client = function(opts){
+  opts = opts || {};
+  opts.useragent = ' up/' + pkg.version;
+  opts.url = opts.remote || 'https://api.cloudup.com';
+  return new Cloudup(opts);
+};
 
 /**
  * Upload stream.
  *
  * @param {Object} opts
  * @return {Stream}
- * @api public
+ * @api private
  */
 
 exports.stream = function(opts){
   opts = opts || {};
-
-  var client = new Cloudup({
-    url: opts.remote || 'https://api.cloudup.com',
-    user: opts.user,
-    pass: opts.pass
-  });
-
+  var client = exports.client(opts);
   var stream = client.stream(opts);
   var Reporter = require('./lib/' + opts.reporter);
   var reporter = new Reporter(stream, opts);
